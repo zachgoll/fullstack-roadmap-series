a66d7fe358c4fd5cd5671a4b472579d4bcb2b0df
Zach Gollwitzer <zach.gollwitzer@gmail.com>
2020-12-22 11:17:35 -0500

    Create challenge-2-solution.js

---
+++ lesson-3/challenge-2-solution.js
@@ -0,0 +1,11 @@
+/* 
+
+There are two things wrong with this line
+1. The variable is invalid.  It should be `someNumber`, not `some Number`.  There is an extra space.
+2. We should be using `let` here because later in the code, we re-assign the value of the variable
+
+*/
+const some Number = 20;
+
+// 3. This line is missing a semi-colon at the end
+someNumber = 50
